61402c8d3b3d5145200aeba8cbc40b9a02714691
Berezentsev Evgeny <jonn09@icloud.com>
2021-05-18 21:38:53 +0300

    Initial Commit

---
+++ SwiftUItest/SwiftUItestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUItestApp.swift
+//  SwiftUItest
+//
+//  Created by Евгений Березенцев on 12.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUItestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
